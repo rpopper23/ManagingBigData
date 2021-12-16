2dd2b4db26121c30768abfc2164b0fa8b8a0642d
r.popper@student.utwente.nl <s2882930@ctit007.ewi.utwente.nl>
2021-12-16 17:29:37 +0100

    ciao

--- Assignment3/WEB/WEB-s2882930-ass3.py
+++ Assignment3/WEB/WEB-s2882930-ass3.py
@@ -1,9 +1,3 @@
-'''
-Ruben Popper: s2882930
-real    3m2.895s
-user    0m12.762s
-sys     0m2.594s
-'''
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
@@ -11,14 +5,16 @@ sc = SparkContext(appName="ass2")
 spark = SparkSession.builder.getOrCreate()
 sc.setLogLevel("ERROR")
 #import files
-df = spark.read.json('/data/doina/WebInsight/2020-07-13/') 
+df = spark.read.json('/data/doina/WebInsight/2020-07-13/')
 df1 = spark.read.json('/data/doina/WebInsight/2020-09-14/')
-    
+
 df = df.select(col('url'), col('fetch')['textSize'].alias('textSize'))
 df1 = df1.select(col('url'), col('fetch')['textSize'].alias('textSize1'))
-    
+
 cond = [df.url == df1.url, (df.textSize > 0) | (df1.textSize1 > 0)]
-    
+
 join_df = df.join(df1, cond, 'inner').select(df.url, (df1.textSize1 - df.textSize).alias("sizediff")).orderBy('sizediff')
-                             
-join_df.coalesce(8).write.format('json').save('file:///home/s2882930/ManagingBigData/Assignment3/WEB/WEB_Results')
+join_df.coalesce(8).write.format('json').save('MBD_Assignments/WEB_Results')
+ 
+
+# /user/s2882930/MBD_Assignments/WEB_Results
