1d65825b5b9d06e6f4feb501b14299a4be5e6c07
ruben popper <rubenpopper@eduvpn110251.mobiel.utwente.nl>
2021-12-17 11:51:42 +0100

    sub

--- Assignment3/WEB/WEB-s2882930-ass3.py
+++ Assignment3/WEB/WEB-s2882930-ass3.py
@@ -8,13 +8,18 @@ sc.setLogLevel("ERROR")
 df = spark.read.json('/data/doina/WebInsight/2020-07-13/')
 df1 = spark.read.json('/data/doina/WebInsight/2020-09-14/')
 
+#select columns of interest
 df = df.select(col('url'), col('fetch')['textSize'].alias('textSize'))
 df1 = df1.select(col('url'), col('fetch')['textSize'].alias('textSize1'))
 
+#define join conditions
 cond = [df.url == df1.url, (df.textSize > 0) | (df1.textSize1 > 0)]
 
+#perform join, create new column containing date difference, and order results by it
 join_df = df.join(df1, cond, 'inner').select(df.url, (df1.textSize1 - df.textSize).alias("sizediff")).orderBy('sizediff')
+
+#reduce number of partitions and save to HDFS
 join_df.coalesce(8).write.format('json').save('MBD_Assignments/WEB_Results')
  
-
+#Path
 # /user/s2882930/MBD_Assignments/WEB_Results
